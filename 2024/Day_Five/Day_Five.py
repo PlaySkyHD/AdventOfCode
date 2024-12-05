7bfc57f0998edc8233bb69c78fc0c32c093c2430
Nic Markfort <nic.markfort@gws.ms>
2024-12-05 09:00:08 +0100

    Add Day5 as Template

---
+++ 2024/Day_Five/Day_Five.py
@@ -0,0 +1,11 @@
+# Part One
+def part_one(file_path):
+    with open(file_path, "r") as file:
+        file_content = file.read()
+part_one(r"2024\Day_Five\example.txt")
+
+# Part Two
+def part_two(file_path):
+    with open(file_path, "r") as file:
+        file_content = file.read()
+part_two(r"2024\Day_Five\example.txt")
